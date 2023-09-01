91001600c7d3fccacf654edefd2b74d1879bc9f8
HarshAggarwal1 <haggarwal2510@gmail.com>
2023-09-02 04:22:45 +0530

    Removed FABs, Added top app bar and bottom app bar and done UI enhancements

--- app/src/main/java/com/accenture/accpenture/Dashboard.java
+++ app/src/main/java/com/accenture/accpenture/Dashboard.java
@@ -3,12 +3,18 @@ package com.accenture.accpenture;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.WindowCompat;
 
 public class Dashboard extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        config();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+    }
+
+    private void config() {
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
     }
 
     @Override
