b1ca53a1a497d8b62c8734f437d250c835568171
HarshAggarwal1 <haggarwal2510@gmail.com>
2023-09-22 14:58:44 +0530

    *

--- app/src/main/java/com/accenture/accpenture/activities/Dashboard.java
+++ app/src/main/java/com/accenture/accpenture/activities/Dashboard.java
@@ -1,6 +1,7 @@
 package com.accenture.accpenture.activities;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.WindowCompat;
@@ -8,9 +9,14 @@ import androidx.core.view.WindowCompat;
 import com.accenture.accpenture.DashboardFragment;
 import com.accenture.accpenture.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 public class Dashboard extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
+    BottomSheetBehavior bottomSheetBehavior;
+    BottomSheetDialog bottomSheetDialog;
+    FrameLayout bottomSheet;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         config();
@@ -24,6 +30,16 @@ public class Dashboard extends AppCompatActivity {
                 .beginTransaction()
                 .replace(R.id.dashboard_fragment_container_view, new DashboardFragment())
                 .commit();
+
+        bottomSheet = findViewById(R.id.bottom_sheet);
+
+        // applying peek height of bottom sheet
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+        bottomSheetBehavior.setPeekHeight(400);
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+
+
+
 
     }
 
